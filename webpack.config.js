1fff7fb7ef4028370a009f3f902b4647cc465c05
gky <3061412170@qq.com>
2022-06-17 20:18:31 +0800

    自动生成html文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 
 const path =require('path')
+
+const HWP=require('html-webpack-plugin')
 module.exports={
     entry:"./src/main.js",//入口
     output:{
         path:path.join(__dirname,'dist'),//路径
         filename:"bundle.js"//出口
-    }
+    },
+    plugins:[
+        new HWP({
+            template: path.join(__dirname, "public/index.html"),
+        })
+    ]
 }
