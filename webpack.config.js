396c89e8d534c54514ed14175be4f99d458be2a8
gky <3061412170@qq.com>
2022-06-20 08:22:57 +0800

    完成

--- webpack.config.js
+++ webpack.config.js
@@ -4,19 +4,27 @@ const path =require('path')
 const HWP=require('html-webpack-plugin')
 
 const { VueLoaderPlugin } = require('vue-loader')
-
+const EslintWebpackPluginq=require('eslint-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { join } = require('path')
 module.exports={
     // development 开发阶段,production 发布阶段
     mode:'development',
     entry:"./src/main.js",//入口
     output:{
         path:path.join(__dirname,'lib'),//路径
-        filename:"index.js"//出口
+        filename:"index.js",//出口
+        clean:true,
     },
     plugins:[
+        new VueLoaderPlugin(),
         new HWP({
             template: path.join(__dirname, "public/index.html"),
-        })
+        }),
+        new EslintWebpackPluginq({
+            context:join(__dirname,'src/')
+        }),
+        new MiniCssExtractPlugin()
     ],
     devServer:{
         port:30000,
@@ -24,10 +32,10 @@ module.exports={
     },
     module:{
         rules:[
-            {
-                test:/\.css$/i,
-                use:["style-loader","css-loader"]
-            },
+            // {
+            //     test:/\.css$/i,
+            //     use:["style-loader","css-loader"]
+            // },
             {
                 test:/\.less$/i,
                 use:["style-loader","css-loader","less-loader"]
@@ -54,11 +62,11 @@ module.exports={
             {
                 test: /\.vue$/,
                 loader: 'vue-loader'
+            },{
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
             }
         ]
     },
-    plugins: [
-        // 请确保引入这个插件！
-        new VueLoaderPlugin()
-    ]
+
 }
